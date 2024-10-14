32740daf4adcf4e406705a7ebd0d6b2f0e962623
Martin Pentrak <mpentrak@redhat.com>
2024-10-14 13:32:43 +0200

    PUT /admin/api/accounts/{id}

--- tests/integration/test_integration_accounts.py
+++ tests/integration/test_integration_accounts.py
@@ -1,4 +1,4 @@
-from tests.integration import asserts
+from tests.integration.asserts import assert_resource, assert_resource_params
 
 
 def test_accounts_list(api, account):
@@ -7,21 +7,28 @@ def test_accounts_list(api, account):
 
 
 def test_account_can_be_created(api, account, account_params):
-    asserts.assert_resource(account)
-    asserts.assert_resource_params(account, account_params)
+    assert_resource(account)
+    assert_resource_params(account, account_params)
+
+
+def test_account_update(api,account,update_account_params):
+    updated_account = account.update(params=update_account_params)
+    assert_resource(updated_account)
+    assert_resource_params(updated_account,update_account_params)
 
 
 def test_account_can_be_read(api, account, account_params):
     read = api.accounts.read(account.entity_id)
-    asserts.assert_resource(read)
-    asserts.assert_resource_params(read, account_params)
+    assert_resource(read)
+    assert_resource_params(read, account_params)
 
 
 def test_account_can_be_read_by_name(api, account, account_params):
     account_name = account['org_name']
     read = api.accounts[account_name]
-    asserts.assert_resource(read)
-    asserts.assert_resource_params(read, account_params)
+    assert_resource(read)
+    assert_resource_params(read, account_params)
+
 
 def test_users_list(api, account):
     assert len(account.users.list()) >= 1
