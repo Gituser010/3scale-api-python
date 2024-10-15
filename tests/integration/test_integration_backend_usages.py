a89c53636006aa396a7ba5bd0aa933ac6e59fcd3
Martin Pentrak <mpentrak@redhat.com>
2024-10-15 17:57:04 +0200

    done /adim/api/service/{id}/backend_usages/{id}

---
+++ tests/integration/test_integration_backend_usages.py
@@ -0,0 +1,18 @@
+from tests.integration import asserts
+from tests.integration.conftest import backend
+from threescale_api.resources import Backend
+
+
+def test_backend_usage_can_be_created(backend_usage, backend_usage_params):
+    asserts.assert_resource(backend_usage)
+    asserts.assert_resource_params(backend_usage, backend_usage_params)
+
+
+def test_backend_usages_list(api, backend_usage, backend):
+    assert  len(backend.usages()) >= 1
+
+
+def test_backend_usage_update(backend_usage, backend, backend_usage_update_params):
+    updated_backend_usage = backend_usage.update(backend_usage_update_params)
+    asserts.assert_resource(updated_backend_usage)
+    asserts.assert_resource_params(updated_backend_usage, backend_usage_update_params)
